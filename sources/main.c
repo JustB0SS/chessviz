927df9d8a64d4accb0d6909ed779d6ed60d30b1b
Игорь <dragonmount176@gmail.com>
2019-04-11 14:19:08 +0700

    Add ability to read text file

---
+++ sources/main.c
@@ -0,0 +1,39 @@
+#include <ctype.h>
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+
+int main(int argc, char* argv[])
+{
+    char board[8][8] = {
+            {'r', 'n', 'b', 'q', 'k', 'b', 'n', 'r'},
+            {'p', 'p', 'p', 'p', 'p', 'p', 'p', 'p'},
+            {'.', '.', '.', '.', '.', '.', '.', '.'},
+            {'.', '.', '.', '.', '.', '.', '.', '.'},
+            {'.', '.', '.', '.', '.', '.', '.', '.'},
+            {'.', '.', '.', '.', '.', '.', '.', '.'},
+            {'P', 'P', 'P', 'P', 'P', 'P', 'P', 'P'},
+            {'R', 'N', 'B', 'Q', 'K', 'B', 'N', 'R'},
+    };
+
+    FILE* input;
+    switch (argc) {
+    case 1:
+        while (1) {
+            // getline()
+        }
+        break;
+    case 2:
+        input = fopen(argv[1], "a+");
+        while (!feof(input)) {
+            char sw[10], sb[10], str[2];
+            fscanf(input, "%s %s %s", str, sw, sb);
+        }
+        break;
+    default:
+        printf("ERROR");
+        return 1;
+    }
+    fclose(input);
+    return 0;
+}
